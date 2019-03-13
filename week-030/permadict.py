c8edd75efe8ea7d8c993ef5bcac2c1ecd21df762
Plugaru Tudor <tudor.plugaru@made.com>
2019-03-13 17:41:34 +0200

    Start working on a solution for permadict

---
+++ week-030/permadict.py
@@ -0,0 +1,26 @@
+from collections.abc import Mapping
+
+class PermaDict(Mapping):
+    def __init__(self, *args, **kwargs):
+        self._data = dict(*args, **kwargs)
+    def __getitem__(self, key):
+        if key == 'ghost':
+            return 'Boo!'
+        return self._data[key]
+    
+    def __iter__(self):
+        return iter(self._data)    # ``ghost`` is invisible
+    
+    def __len__(self):
+        return len(self._data)
+        
+    def __setitem__(self, key, value):
+        if key in self._data:
+            raise KeyError(f"'{key}' already in dictionary.")
+        self._data[key] = value
+    
+    def update(self, **kwargs):
+        self.__setitem__(**kwargs)
+    
+    def force_set(self, key, value):
+        self._data[key] = value
